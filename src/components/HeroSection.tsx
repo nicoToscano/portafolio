7a65582e2db69b3821514f7319d50662595dd91b
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-23 19:48:26 -0500

    Actualización de la sección Hero: cambio de saludo y mejora de la descripción profesional en español.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -41,14 +41,14 @@ export default function HeroSection() {
               variants={childVariants}
             >
               {personalInfo.name}{" "}
-              <span className="inline-block animate-pulse">✨</span>
+              <span className="inline-block animate-pulse">👋🏻</span>
             </motion.h1>
 
             <motion.p
               className="text-xl text-muted-foreground mb-6"
               variants={childVariants}
             >
-              Software Engineer 👨‍💻
+              Ingeniero de Software 💻
             </motion.p>
 
             <motion.div
@@ -122,15 +122,7 @@ export default function HeroSection() {
           <div className="bg-gradient-to-r from-purple-500/10 to-pink-500/10 backdrop-blur-sm backdrop-filter p-4 rounded-lg border border-purple-500/20 dark:border-purple-500/10 shadow-sm">
             <p className="text-muted-foreground pl-4 py-2 mb-4 relative">
               <span className="absolute left-0 top-0 h-full w-1 bg-gradient-to-b from-purple-500 to-pink-500 rounded-full"></span>
-              🚀 Passionate software engineer with a versatile skill set
-              spanning multiple domains. I thrive on solving complex challenges
-              across different platforms and environments, adapting quickly to
-              new technologies and methodologies. My holistic approach combines
-              technical expertise with creative problem-solving, allowing me to
-              develop solutions that are both innovative and practical. I'm
-              driven by continuous learning and a commitment to excellence,
-              whether working independently or collaborating with diverse teams
-              to create impactful, scalable solutions.
+              🚀 Ingeniero de Software apasionado por la programación, con interés en el desarrollo tanto Frontend como Backend. Tengo experiencia práctica con tecnologías modernas como React, Astro, Tailwind y Supabase, y actualmente estoy profundizando mis conocimientos en frameworks como Next.js y NestJS. Aunque mi experiencia en .NET y Java es básica, tengo una fuerte disposición para aprender y adaptarme rápidamente a nuevas tecnologías. Actualmente busco seguir creciendo profesionalmente y contribuir a proyectos innovadores que representen un desafío técnico y personal.
             </p>
           </div>
         </MotionWrapper>
